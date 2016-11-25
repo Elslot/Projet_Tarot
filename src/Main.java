75d61fc4b9df9156c13fef6149e3aea3b03c0b76
Kilian Arnaudeau <kilian.arnaudeau@etu.u-bordeaux.fr>
2016-11-25 13:28:09 +0100

    Ajout du Main, correction de la classe View

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by karnaudeau on 23/11/16.
+ */
+public class Main extends Application{
+
+    public void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
--- src/View.java
+++ src/View.java
@@ -1,4 +1,12 @@
+import javafx.stage.Window;
 
-public class View {
+import java.util.Observable;
+import java.util.Observer;
 
+public class View extends Window implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
 }
