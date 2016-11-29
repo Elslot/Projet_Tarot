6e8c7c9ef496ec7b294a6862c57a970a6b65e5cf
Kilian Arnaudeau <kilian.arnaudeau@etu.u-bordeaux.fr>
2016-11-29 18:07:39 +0100

    Commencement View

--- src/View.java
+++ src/View.java
@@ -1,9 +1,26 @@
-import javafx.stage.Window;
+import javafx.scene.input.KeyCombination;
+import javafx.stage.Stage;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class View extends Window implements Observer {
+public class View extends Stage implements Observer {
+
+    protected static final int windowSizeW = 400;
+    protected static final int windowSizeH = 100;
+
+    protected Stage Fenetre;
+
+    protected Controller c;
+
+    public View(){
+
+        Fenetre = new Stage();
+        Fenetre.setFullScreen(true);
+        Fenetre.setFullScreenExitHint("Press ESC to exit FullScreen Mode");
+
+
+    }
 
     @Override
     public void update(Observable o, Object arg) {
